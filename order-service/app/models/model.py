5cd3630cdeba5ba5f1b5cbbaa872edf4f923a820
Maksim Galkin <msgalkin@edu.hse.ru>
2024-11-22 18:54:12 +0300

    Some changes in gRPC logic and table schema

--- order-service/app/models/model.py
+++ order-service/app/models/model.py
@@ -2,13 +2,16 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+
 class AssignedOrder(BaseModel):
     assign_order_id: str
     order_id: str
     executer_id: str
-    coin_coeff: float
-    coin_bonus_amount: float
+    base_coin_amount: float
+    coin_coef: float
+    bonus_amount: float
     final_coin_amount: float
     route_information: str
     assign_time: datetime
     acquire_time: Optional[datetime]
+    is_canceled: bool
--- source-service/app/services/grpc_server_runner.py
+++ source-service/app/services/grpc_server_runner.py
@@ -1,54 +1,43 @@
 import grpc
 from concurrent import futures
 import time
-from grpc_server import data_requests_pb2_grpc, data_requests_pb2
+from grpc_server import data_requests_pb2_grpc
+from grpc_server.data_requests_pb2 import GetOrderInfoResponse, OrderPrice, ExecuterProfile
 from services import data_requests
 
 
 class DataRequestsService(data_requests_pb2_grpc.DataRequestsServiceServicer):
     """Implementation of the gRPC methods defined in data_requests.proto."""
 
-    def GetOrderData(self, request, context):
-        # Retrieve order data based on order_id from the request
+    def GetOrderInfo(self, request, context):
         order_data = data_requests.get_order_data(request.order_id)
-        return data_requests_pb2.OrderData(
-            id=order_data.id,
-            user_id=order_data.user_id,
-            zone_id=order_data.zone_id,
-            base_coin_amount=order_data.base_coin_amount
-        )
 
-    def GetZoneInfo(self, request, context):
-        # Retrieve zone information based on zone_id from the request
-        zone_info = data_requests.get_zone_info(request.zone_id)
-        return data_requests_pb2.ZoneData(
-            id=zone_info.id,
-            coin_coeff=zone_info.coin_coeff,
-            display_name=zone_info.display_name
-        )
+        zone_info = data_requests.get_zone_info(order_data.zone_id)
 
-    def GetExecuterProfile(self, request, context):
-        # Retrieve executer profile based on executer_id from the request
+        toll_roads = data_requests.get_toll_roads(zone_info.display_name)
+
         executer_profile = data_requests.get_executer_profile(request.executer_id)
-        return data_requests_pb2.ExecuterProfile(
-            id=executer_profile.id,
-            tags=executer_profile.tags,
-            rating=executer_profile.rating
-        )
 
-    def GetConfigs(self, request, context):
-        # Retrieve configuration settings
-        print("hi from GetConfigs")
         configs = data_requests.get_configs()
-        return data_requests_pb2.ConfigData(
-            coin_coeff_settings=configs.coin_coeff_settings
-        )
 
-    def GetTollRoads(self, request, context):
-        # Retrieve toll roads data based on zone_display_name from the request
-        toll_roads = data_requests.get_toll_roads(request.zone_display_name)
-        return data_requests_pb2.TollRoadsData(
-            bonus_amount=toll_roads.bonus_amount
+        actual_coin_coeff = zone_info.coin_coeff
+        if configs.coin_coeff_settings:
+            actual_coin_coeff = min(float(configs.coin_coeff_settings.get('maximum', '1')), actual_coin_coeff)
+        final_order_price = order_data.base_coin_amount * actual_coin_coeff + toll_roads.bonus_amount
+
+        return GetOrderInfoResponse(
+            order_price=OrderPrice(
+                base_order_price=order_data.base_coin_amount,
+                coin_coef=zone_info.coin_coeff,
+                coin_bonus_amount=toll_roads.bonus_amount,
+                final_order_price=final_order_price
+            ),
+            zone_display_name=zone_info.display_name,
+            executor_profile=ExecuterProfile(
+                id=executer_profile.id,
+                tags=executer_profile.tags,
+                rating=executer_profile.rating
+            )
         )
 
 
